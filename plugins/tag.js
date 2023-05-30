80ca8e7b2139417eb352a621985c2e57420ed8e5
DOOZY LAB LK <93440107+TEAM-DLK@users.noreply.github.com>
2023-05-31 04:55:37 +0530

    Update tag.js
    TEAM-DLK-MD

--- plugins/tag.js
+++ plugins/tag.js
@@ -1,20 +1,15 @@
-/* Copyright (C) 2022 Sourav KL11.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
-*/
 const {Module} = require('../main')
 const {getString} = require('./misc/lang');
 const {readFileSync} = require('fs');
 const {saveMessage} = require('./misc/saveMessage');
 const Lang = getString('group');
 Module({pattern: 'tag ?(.*)',use: 'group', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
-if (match[1] === "all" || (match[1] && match[1].startsWith("ad")) || !message.reply_message) return;
+if (match[1] === "all" || !message.reply_message) return;
 var target = message.jid
-if (match[1] && /[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g.test(match[1])) target = [...match[1].match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)];
+if (match[1] && /[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g.test(match[1])) target = [...match[1].match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)][0];
 var group = await message.client.groupMetadata(target)
 var jids = [];
-if (match[1] == 'admin'){
+if (match[1].includes('admin')){
         var admins = group.participants.filter(v => v.admin !== null).map(x => x.id);
         admins.map(async (user) => {
             jids.push(user.replace('c.us', 's.whatsapp.net'));
